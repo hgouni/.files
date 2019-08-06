d86b73ce96f9a8b70e6c08a4843cd92b4908ab7c
Hemant Sai Gouni <hemant@hemantgouni.com>
2019-08-06 18:00:48 +0000

    add -q flag to fully suppress grep output

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -15,7 +15,7 @@
 # automatically configure bash to replace itself with fish process while preserving login scripts
 # allow 'bash -c' to be used without passing --norc
 if string match '*bash*' $SHELL >/dev/null 2>&1
-    and not command grep -s "exec fish" $HOME/.bashrc
+    and not command grep -qs "exec fish" $HOME/.bashrc
     printf '%s\n' 'if [ -z "$BASH_EXECUTION_STRING" ]; then exec fish; fi' >> $HOME/.bashrc
 end
 
