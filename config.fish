24ef8bd838f5b5c0560ebdb5d7a21d6a0430224a
Hemant Sai Gouni <hemant@hemantgouni.com>
2019-08-06 08:49:41 +0000

    large update to dotfiles. see individual files

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 ### INITIAL SHELL SETUP ###
 
-# Install Fisher: 
+# Install Fisher:
 # Plugins:
 # jethrokuan/z
 # edc/bass
@@ -10,8 +10,20 @@
 #     fish -c fisher
 # end
 
+# Note: run fish_config to choose colorscheme
+
+# automatically configure bash to replace itself with fish process while preserving login scripts
+# allow 'bash -c' to be used without passing --norc
+if string match '*bash*' $SHELL >/dev/null 2>&1
+    and not command grep -q "exec fish" $HOME/.bashrc
+    printf '%s\n' 'if [ -z "$BASH_EXECUTION_STRING" ]; then exec fish; fi' >> $HOME/.bashrc
+end
+
 # Suppress greeting
 set fish_greeting
+
+# Minimize escape delay (for vi mode)
+set -g fish_escape_delay_ms 10
 
 ### KEYBINDINGS ###
 
@@ -23,51 +35,96 @@ set fish_cursor_replace_one underscore
 
 # General keybind function
 function fish_user_key_bindings
-	bind -M insert \el accept-autosuggestion 
-	bind -M insert ! bind_bang
-	bind -M insert '$' bind_dollar
+    # can i make it escape afterwards, to emulate the expected alt-<key> behavior?
+    # forward-char works, too
+    bind -M insert \el accept-autosuggestion
+    bind -M insert ! bind_bang
+    bind -M insert '$' bind_dollar
 end
 
 # These two functions enable history substitution functionality
 function bind_bang
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
+    switch (commandline -t)
+    case "!"
+        commandline -t $history[1]; commandline -f repaint
+    case "*"
+        commandline -i !
+    end
 end
 
 function bind_dollar
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
+    switch (commandline -t)
+    case "!"
+        commandline -t ""
+        commandline -f history-token-search-backward
+    case "*"
+        commandline -i '$'
+    end
+end
+
+# function for safe rm; moves files to xdg trash using trash-cli or moves removed files to ~/.trash
+# must manually invoke /bin/rm for destructive actions
+# gnu mv with -b flag creates backups of duplicate file names
+function rm
+    if type -q trash-put
+        command trash-put $argv
+    else
+        command mkdir -p "$HOME/.trash";
+        and command mv -b $argv "$HOME/.trash"
+    end
+end
+
+### ALIASES/ABBREVIATIONS ###
+
+# general abbreviation function
+if status --is-interactive
+    abbr --add --global rb bash -c
+    abbr --add --global bl tput bel
+    abbr --add --global mv mv -b
+    abbr --add --global rmls trash-list
+    abbr --add --global unrm trash-restore
+    abbr --add --global rmrm trash-rm
 end
 
 ### PROMPT ###
 
-# Emulate sorin prompt
+# emulate sorin prompt
 function fish_prompt
-	# Show exit code
-	set last_status $status
-	echo -n (set_color purple)$last_status' '
+    # show exit code if nonzero
+    # it's necessary to first save $status to last_status because it will get overwritten
+    # by the test statement otherwise. If this is not done, the nonzero exit code will be
+    # detected successfully, but the exit code will display as zero
+    set last_status $status
+    if test "$last_status" -ne 0
+        printf '%s' (set_color purple)"$last_status"' '(set_color normal)
+    end
 
-	# Show username and hostname if ssh
-	test $SSH_TTY
-	and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
-	test "$USER" = 'root'
-	and echo (set_color red)"#"
+    # set prompt ending character before ssh detection
+    set prompt_end '❯'
 
-	# Prompt end
-	echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '(set_color normal)
+    # detect if running in a virtual terminal (note: figure out string escaping in
+    # command sub); can also use fgconsole here
+    # show username and hostname if ssh or vt
+    if string match '*tty*' (tty) >/dev/null 2>&1
+        or test -n "$SSH_TTY" -o -n "$SSH_CLIENT"
+        printf '%s' (set_color red)"$USER"(set_color yellow)'@'(set_color green)(prompt_hostname)' '(set_color normal)
+        set prompt_end '>'
+    end
+
+    # indicate sudo status
+    if command sudo -n true >/dev/null 2>&1
+        printf '%s' (set_color red)(prompt_pwd)' '(set_color normal)
+    else
+        printf '%s' (set_color cyan)(prompt_pwd)' '(set_color normal)
+    end
+
+    # prompt end
+    printf '%s' (set_color red)"$prompt_end"(set_color yellow)"$prompt_end"(set_color green)"$prompt_end"' '(set_color normal)
 end
 
 # create git prompt
-set __fish_git_prompt_showdirtystate 'yes'
+# showdirtystate is too slow
+# set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream auto
@@ -76,47 +133,57 @@ set __fish_git_prompt_showcolorhints
 
 # set right prompt to show command execution time and git status
 function fish_right_prompt
-	# execution time
-	if test $CMD_DURATION
-		# change time color based on execution time
-		set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
-		if test $CMD_DURATION -lt 5000
-			echo (set_color green)$duration(set_color normal)
-		end
-		if test $CMD_DURATION -lt 60000; and test $CMD_DURATION -ge 5000
-			echo (set_color yellow)$duration(set_color normal)
-		end
-		if test $CMD_DURATION -ge 60000
-			echo (set_color red)$duration(set_color normal)
-		end
-	end
-	# git status
-	__fish_git_prompt
-end
+    # git status
+    __fish_git_prompt
+    printf '%s' ' '
 
-### ALIASES/ABBREVIATIONS ###
-
-# general abbreviation function
-if status --is-interactive
-	set -g fish_user_abbreviations
-	abbr -a b bash -c
+    # execution time; posix-compliant test statement ensures $CMD_DURATION is not null
+    if test "$CMD_DURATION"
+        # divide command duration by 1000 and print to three decimal places
+        set duration (printf '%s\n' "$CMD_DURATION 1000" | awk '{printf "%.3f", $1 / $2}')
+        # change time color based on execution time
+        if test "$CMD_DURATION" -lt 5000
+            printf '%s' (set_color green)"$duration"(set_color normal)
+        else if test "$CMD_DURATION" -ge 5000; and test "$CMD_DURATION" -lt 60000
+            printf '%s' (set_color yellow)"$duration"(set_color normal)
+        else
+            printf '%s' (set_color red)"$duration"(set_color normal)
+        end
+    end
 end
 
 ### ENVIRONMENT VARIABLES ###
+
+# add local dir to PATH
+if test -d "$HOME/.local/bin"
+    and not contains $HOME/.local/bin $PATH
+    set PATH $HOME/.local/bin $PATH
+end
+
+# add ghc and friends to path (needed for hie to function correctly); using universal var for speed
+# we're not going to use contains here because that would require invoking stack, which is slow
+if type -q stack
+    and not type -q ghc
+    set -U fish_user_paths (command stack path --compiler-bin) $fish_user_paths
+end
 
 # --files: List files that would be searched but do not search
 # --no-ignore: Do not respect .gitignore, etc...
 # --hidden: Search hidden files and folders
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
-set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+if type -q rg
+    set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git"'
+end
 
 # faster than having completion tools etc autodetect it
-set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
+if type -q rustc
+    and test -z "$RUST_SRC_PATH"
+    set -Ux RUST_SRC_PATH (command rustc --print sysroot)/lib/rustlib/src/rust/src
+end
 
 # set editor env vars
-set -x VISUAL nvim
-set -x EDITOR $VISUAL
-
-# make sure fish is set as default shell
-set -x SHELL /bin/fish
+if type -q nvim
+    set -x VISUAL nvim
+    set -x EDITOR $VISUAL
+end
--- init.vim
+++ init.vim
@@ -1,6 +1,7 @@
-" vim needs a more posix-compatible shell than fish
+"vim needs a more posix-compatible shell than fish
 if &shell =~# 'fish$'
-	set shell=bash
+    "from vim-sensible
+    set shell=/usr/bin/env\ bash
 endif
 
 "autoinstall plug
@@ -11,15 +12,31 @@ if empty(glob('~/.local/share/nvim/site/autoload/plug.vim'))
 endif
 
 "dependencies:
-"ripgrep
-"rls
-"python for neovim?
+"generic:
+"python for neovim? (pip3 install --user --upgrade pynvim)
+"universal-ctags (pull from git repo, build and install in $PATH)
+"ripgrep (sudo dnf install ripgrep)
+"language-specific:
+"rust:
+"rust (curl https://sh.rustup.rs -sSf | sh)
+"rls (rustup component add rls rust-analysis rust-src)
+"rustfmt (rustup component add rustfmt)
+"ctags (compile/install from source)
+"racket/sicp:
+"download racket from racket-lang.org
+"use ddg, idk
+"haskell:
+"hie
+"install stack then follow source install instructions on github
+"install hfmt and all of hfmt fixers
 
 call plug#begin()
 
+Plug 'justinmk/vim-sneak'
+
 Plug 'morhetz/gruvbox'
 
-Plug 'w0rp/ale'
+Plug 'dense-analysis/ale'
 
 Plug 'vim-airline/vim-airline'
 
@@ -27,7 +44,7 @@ Plug 'ervandew/supertab'
 
 Plug 'mbbill/undotree'
 
-Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
+Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --bin' }
 
 Plug 'junegunn/fzf.vim'
 
@@ -35,17 +52,40 @@ Plug 'tpope/vim-commentary'
 
 Plug 'tpope/vim-surround'
 
+Plug 'tpope/vim-repeat'
+
 Plug 'dag/vim-fish'
 
 Plug 'vimwiki/vimwiki'
 
+Plug 'junegunn/rainbow_parentheses.vim'
+
+Plug 'wlangstroth/vim-racket'
+
+Plug 'airblade/vim-gitgutter'
+
+Plug 'majutsushi/tagbar'
+
+Plug 'tpope/vim-fugitive'
+
+Plug 'rust-lang/rust.vim'
+
+Plug 'neovimhaskell/haskell-vim'
+
 call plug#end()
+
+"note: there's no need to create an alternative escape-- alt+space will
+"already do this (alt+char creates terminal escape code that executes
+"<esc> + <char>)
 
 "disable modelines (security)
 set nomodeline
 
 "leader key
 let mapleader = " "
+
+"localleader key; '\\' must be used because '\' functions as escape char
+let maplocalleader = "\\"
 
 "line numbers
 set number
@@ -57,6 +97,37 @@ set scrolloff=5
 "move preview window to bottom (less intrusive)
 set splitbelow
 
+"make cursor more visible"
+set cursorline
+
+"toggle highlighting for searches
+set hlsearch!
+nnoremap <silent><leader>/ :set hlsearch!<CR>
+
+"tab settings
+set expandtab
+set tabstop=4
+set shiftwidth=4
+
+" automatically close things
+inoremap " ""<left>
+inoremap ' ''<left>
+inoremap ( ()<left>
+inoremap [ []<left>
+inoremap { {}<left>
+inoremap {<CR> {<CR>}<ESC>O
+inoremap {;<CR> {<CR>};<ESC>O
+" skip past inserted characters
+inoremap <C-s> <C-o>A
+
+" special symbols
+inoremap \forall ∀
+inoremap \to →
+inoremap \lambda λ
+inoremap \Sigma Σ
+inoremap \exists ∃
+inoremap \equiv ≡
+
 "last position jump
 au BufReadPost *
   \ if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit'
@@ -64,7 +135,35 @@ au BufReadPost *
   \ | endif
 
 "esc returns to normal in terminal
+"note: use alt + j or something similar to switch to normal mode in fish
 tnoremap <Esc> <C-\><C-n>
+
+"toggle terminal
+let s:term_buf = 0
+let s:term_win = 0
+
+function! Term_toggle(height)
+    if win_gotoid(s:term_win)
+        hide
+    else
+        new terminal
+        exec "resize ".a:height
+        try
+            exec "buffer ".s:term_buf
+            exec "bd terminal"
+        catch
+            call termopen($SHELL, {"detach": 0})
+            let s:term_buf = bufnr("")
+            setlocal nocursorline "signcolumn=no
+        endtry
+        startinsert!
+        let s:term_win = win_getid()
+    endif
+endfunction
+
+nnoremap <silent><M-t> :call Term_toggle(10)<CR>
+inoremap <silent><M-t> <ESC>:call Term_toggle(10)<CR>
+tnoremap <silent><M-t> <C-\><C-n>:call Term_toggle(10)<CR>
 
 "theming
 set termguicolors
@@ -72,15 +171,41 @@ set background=dark
 let g:gruvbox_contrast_dark = 'medium'
 colorscheme gruvbox
 
+"persistent undo and undo tree config (not necessary to specify undodir)
+set undofile
+let g:undotree_WindowLayout = 3
+let g:undotree_ShortIndicators = 1
+let g:undotree_HighlightChangedText = 0
+let g:undotree_HelpLine = 0
+let g:undotree_SetFocusWhenToggle = 1
+nnoremap <leader>u :UndotreeToggle<CR>
+
+"enable jumping to hints
+let g:sneak#label = 1
+
 "ALE config
 let g:ale_completion_enabled = 1
+let g:ale_fix_on_save = 1
 let g:ale_linters = {
-	\ 'rust': [ 'rls' ],
-	\ }
+    \ 'rust': ['rls'],
+    \ 'haskell': ['hie'],
+    \ }
+let g:ale_fixers = {
+    \ '*': ['remove_trailing_lines', 'trim_whitespace'],
+    \ 'rust': ['rustfmt'],
+    \ 'haskell': ['hfmt'],
+    \ }
 let g:ale_rust_rls_toolchain = 'stable'
+let g:ale_haskell_hie_executable = 'hie-wrapper'
 
 "fzf config
-noremap <leader>f :Files<CR>
+nnoremap <leader>ff :Files<CR>
+nnoremap <leader>fb :BLines<CR>
+nnoremap <leader>fl :Lines<CR>
+nnoremap <leader>ft :BTags<CR>
+nnoremap <leader>fp :Tags<CR>
+nnoremap <leader>fm :Marks<CR>
+nnoremap <leader>fc :Commands<CR>
 command! -bang -nargs=* Rg
   \ call fzf#vim#grep(
   \   'rg --hidden --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
@@ -102,16 +227,13 @@ let g:fzf_colors =
   \ 'spinner': ['fg', 'Label'],
   \ 'header':  ['fg', 'Comment'] }
 
-"persistent undo and undo tree config
-set undofile
-let g:undotree_WindowLayout = 3
-let g:undotree_ShortIndicators = 1
-let g:undotree_HighlightChangedText = 0
-let g:undotree_HelpLine = 0
-let g:undotree_SetFocusWhenToggle = 1
-noremap <leader>u :UndotreeToggle<CR>
-
 " airline configuration
+if !exists('g:airline_symbols')
+    let g:airline_symbols = {}
+endif
+let g:airline_symbols.branch = '⎇'
+let g:airline_symbols.notexists = '!'
+let g:airline#extensions#hunks#enabled = 0
 let g:airline#extensions#tabline#enabled = 1
 let g:airline#extensions#tabline#buffer_idx_mode = 1
 nmap <leader>1 <Plug>AirlineSelectTab1
@@ -125,3 +247,55 @@ nmap <leader>8 <Plug>AirlineSelectTab8
 nmap <leader>9 <Plug>AirlineSelectTab9
 nmap <leader>- <Plug>AirlineSelectPrevTab
 nmap <leader>= <Plug>AirlineSelectNextTab
+
+"tagbar config
+nnoremap <leader>t :TagbarToggle<CR>
+let g:rust_use_custom_ctags_defs = 1  " if using rust.vim
+let g:tagbar_type_rust = {
+  \ 'ctagsbin' : '/home/user/.local/bin/ctags',
+  \ 'ctagstype' : 'rust',
+  \ 'kinds' : [
+      \ 'n:modules',
+      \ 's:structures:1',
+      \ 'i:interfaces',
+      \ 'c:implementations',
+      \ 'f:functions:1',
+      \ 'g:enumerations:1',
+      \ 't:type aliases:1:0',
+      \ 'v:constants:1:0',
+      \ 'M:macros:1',
+      \ 'm:fields:1:0',
+      \ 'e:enum variants:1:0',
+      \ 'P:methods:1',
+  \ ],
+  \ 'sro': '::',
+  \ 'kind2scope' : {
+      \ 'n': 'module',
+      \ 's': 'struct',
+      \ 'i': 'interface',
+      \ 'c': 'implementation',
+      \ 'f': 'function',
+      \ 'g': 'enum',
+      \ 't': 'typedef',
+      \ 'v': 'variable',
+      \ 'M': 'macro',
+      \ 'm': 'field',
+      \ 'e': 'enumerator',
+      \ 'P': 'method',
+  \ },
+\ }
+
+" rainbow parenthesis config (for lisp)
+augroup rainbow_lisp
+    autocmd!
+    autocmd FileType lisp,clojure,scheme RainbowParentheses
+augroup END
+
+" haskell-vim configuration
+let g:haskell_enable_quantification = 1
+let g:haskell_enable_recursivedo = 1
+let g:haskell_enable_arrowsyntax = 1
+let g:haskell_enable_pattern_synonyms = 1
+let g:haskell_enable_typeroles = 1
+let g:haskell_enable_static_pointers = 1
+let g:haskell_backpack = 1
