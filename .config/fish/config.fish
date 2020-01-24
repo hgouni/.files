3f798281712ff405bb357934f87c1957ffff71f6
Hemant Sai Gouni <hemant@hemantgouni.com>
2020-01-24 01:21:34 +0000

    update formats

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -254,7 +254,6 @@ end
 set -x QUBES_GPG_DOMAIN gpg-vault
 
 # --files: List files that would be searched but do not search
-# --no-ignore: Do not respect .gitignore, etc...
 # --hidden: Search hidden files and folders
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ and Trash/ folders)
