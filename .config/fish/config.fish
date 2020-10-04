931331a9c04f1fc9be9f2a9e3e1f8af8c5c7d460
Hemant Sai Gouni <hemant@hemantgouni.com>
2020-10-04 23:44:33 +0000

    add fish keybinds

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -71,7 +71,8 @@ end
 function fish_user_key_bindings
     bind -M insert -m default \el accept-autosuggestion repaint-mode
     bind -M insert -m default \ew forward-word repaint-mode
-    bind -M insert -m default \er 'commandline $history[1]; and __fish_prepend_sudo; and commandline -f repaint-mode'
+    bind -M insert -m default \es 'commandline $history[1]; and __fish_prepend_sudo; and commandline -f repaint-mode'
+    bind -M insert -m default \er 'commandline -t ""; and commandline -f history-token-search-backward; and commandline -f repaint-mode'
     bind -M default -m insert a 'commandline -C (math (commandline -C) + 1); and commandline -f repaint-mode'
     bind -M default w forward-word
     bind -M default u undo
@@ -169,7 +170,9 @@ end
 function tmgmt
     switch "$argv[1]"
     case -s
-        nohup st ssh $argv[2..-1] &
+        nohup st ssh $argv[2..-1] > /dev/null 2>&1 &
+    case -m
+        nohup st mosh $argv[2..-1] > /dev/null 2>&1 &
     case -d
         for line in (tmux list-sessions | grep -Ev '\(attached\)$' | cut -d : -f 1)
             tmux kill-session -t $line
