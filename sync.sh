cc3febd5e20ae306df0252b08c53e4048893f7b9
Hemant Sai Gouni <hemant@hemantgouni.com>
2022-05-23 22:09:26 +0000

    The rsync command was a bit wrong :(

--- sync.sh
+++ sync.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# exclude files that should not be uploaded to git
+# like private keys for VPN configs
 rsync -av --delete --exclude 'secure' /etc/nixos/ ./etc/nixos
+
+# exclude everything but .fnl files in any directory in the
+# nvim one. do not include empty directories
 rsync -av --delete --prune-empty-dirs \
       --include '*.fnl' \
       --include "*/" \
       --exclude "*" \
-      "$HOME/.config/nvim" '.config/nvim'
+      "$HOME/.config/nvim/" '.config/nvim'
