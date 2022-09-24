4a94215ba0cc6f64873ac35a498732e4d045a93d
Hemant Sai Gouni <hemant@hemantgouni.com>
2022-09-24 06:09:20 +0000

    Simplify script to compile nvim config

--- compile-nvim-config.sh
+++ compile-nvim-config.sh
@@ -5,12 +5,6 @@ set -u          # treat uninitialized variables as an error
 set -o pipefail # pipes return error if any commands do
 
 for file in $(find "$HOME/.config/nvim" -type f -name '*.fnl'); do
-
     fennel --compile "$file" | tee "$(printf '%s\n' $file | sed -e 's/fnl/lua/g')" > /dev/null
-
-    if [ "$?" -eq 0 ]; then
-        printf '%s compiled\n' $file
-    else
-        printf 'error compiling %s\n' $file
-    fi
+    printf '%s compiled\n' $file
 done
