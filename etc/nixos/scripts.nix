9593e351ffae2794618b9ffe367c1ec0574d1bb2
Hemant Sai Gouni <me@hgouni.com>
2023-06-18 02:59:35 +0000

    update update script :)

--- etc/nixos/scripts.nix
+++ etc/nixos/scripts.nix
@@ -6,24 +6,43 @@
     ''
       set -euo pipefail
 
+      reboot='no'
+
+      while test "$#" -gt 0
+      do
+          case "$1" in
+              '-h' | '--help')
+                  printf '%s\n%s\n%s\t%s\n' \
+                      'Usage: update [-r]' \
+                      'Update nixos and reboot.' \
+                      '-r, --reboot' 'Reboot without prompting.'
+                  exit
+                  ;;
+              '-r' | '--reboot')
+                  reboot='yes'
+                  ;;
+              *)
+                  printf 'Invalid options passed: %s\n' "$1"
+                  exit
+                  ;;
+          esac
+          shift
+      done
+
       sudo nix flake update /etc/nixos
 
       sudo nixos-rebuild boot
 
-      if test "$#" -gt 0
+      if test "$reboot" = 'yes'
       then
-          case "$1" in
-              -r)
-                  reboot
-                  exit
-                  ;;
-          esac
+          reboot
+          exit
       fi
 
       read -rp 'Reboot now? [y/n] ' answer
 
       case "$answer" in
-          y | Y)
+          'y' | 'Y')
               reboot
               ;;
           *)
