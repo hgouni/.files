601419e636a7589099e8000d0b2e0f9afcc6a0c8
Hemant Sai Gouni <me@hgouni.com>
2023-06-19 23:54:59 +0000

    update update script

--- etc/nixos/scripts.nix
+++ etc/nixos/scripts.nix
@@ -6,44 +6,79 @@
     ''
       set -euo pipefail
 
-      reboot='no'
+      action='noop'
+      method='boot'
 
-      while test "$#" -gt 0
+      # nested quotes here aren't truly nested-- command substitution begins a new
+      # evaluation context, so the outer quotes don't apply and we don't need to
+      # escape the inner ones
+      argv_0="$(basename "$0")"
+
+      set_action () {
+          if test "$action" == 'noop'
+          then
+              action="$1"
+          else
+              printf 'Cannot set more than one of -p, -r, and -s. Exiting.\n'
+              exit
+          fi
+      }
+
+      while getopts 'hrspi' opt
       do
-          case "$1" in
-              '-h' | '--help')
-                  printf '%s\n%s\n%s\t%s\n' \
-                      'Usage: update [-r]' \
+          case "$opt" in
+              '?' | 'h')
+                  printf '%s\n%s\n%s\t%s\n%s\t%s\n%s\t%s\n%s\t%s\n' \
+                      "Usage: $argv_0 [-prsi]" \
                       'Update nixos and reboot.' \
-                      '-r, --reboot' 'Reboot without prompting.'
+                      '-p' 'Suspend without prompting.' \
+                      '-r' 'Reboot without prompting.' \
+                      '-s' 'Shutdown without prompting.' \
+                      '-i' 'Immediately switch to the updated system.'
                   exit
                   ;;
-              '-r' | '--reboot')
-                  reboot='yes'
+              'r')
+                  set_action 'reboot'
                   ;;
-              *)
-                  printf 'Invalid options passed: %s\n' "$1"
-                  exit
+              's')
+                  set_action 'shutdown now'
+                  ;;
+              'p')
+                  set_action 'systemctl suspend'
+                  ;;
+              'i')
+                  method='switch'
                   ;;
           esac
-          shift
       done
+
+      # removes all parsed options
+      shift "$((OPTIND - 1))"
 
       sudo nix flake update /etc/nixos
 
-      sudo nixos-rebuild boot
+      sudo nixos-rebuild "$method"
 
-      if test "$reboot" = 'yes'
+      # could just run action here after setting it to ':' (bash noop) at the
+      # beginning instead of testing first, but just in case action is ever something
+      # that doesn't immediately stop execution (shutdown or reboot)...
+      if test "$action" != 'noop'
       then
-          reboot
+          $action
           exit
       fi
 
-      read -rp 'Reboot now? [y/n] ' answer
+      read -rp 'Suspend, reboot or shutdown now? [p/r/s/n] ' answer
 
       case "$answer" in
-          'y' | 'Y')
+          'p')
+              systemctl suspend
+              ;;
+          'r')
               reboot
+              ;;
+          's')
+              shutdown
               ;;
           *)
               printf 'Exiting.\n'
