b7a6ebceb9005e34e5bdd2bb9eb6ce48f42123d1
Hemant Sai Gouni <me@hgouni.com>
2023-06-24 17:32:41 +0000

    Don't specify git signing key as file

--- etc/nixos/git.nix
+++ etc/nixos/git.nix
@@ -11,7 +11,8 @@
             commit.gpgsign = true;
             tag.gpgsign = true;
             gpg.format = "ssh";
-            user.signingkey = "/etc/nixos/files/ssh/id_ed25519_sk.pub";
+            # key:: prefix required for gpg.format = "ssh"
+            user.signingkey = "key::sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKUw9q0rmmYPBXUHQhdSwG1/4ap0Fypm5J+s6rOch0byAAAABHNzaDo=";
         };
     };
 }
