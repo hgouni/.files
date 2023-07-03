cfadf6b6fdf36227004e91f928dfcd16758fa84e
Hemant Sai Gouni <me@hgouni.com>
2023-07-03 07:43:00 +0000

    sync-dotfiles.sh: better status reporting

--- etc/nixos/files/scripts/sync-dots.sh
+++ etc/nixos/files/scripts/sync-dots.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-# @ --exclude 'secure' \:
-# exclude files that should not be uploaded to git
-# like private keys for VPN configs
+printf 'Syncing /etc/nixos\n'
+
+# --prune-empty-dirs:
+# prevent empty directories from being created at the destination (as a result
+# of exclusions)
+#
+# --exclude 'secure':
+# exclude files that should not be uploaded to git, like private keys for VPN
+# configs
 # 
 # (this comment can't go in the rsync command itself, gets parsed wrong)
-rsync -av --delete \
+rsync --info=NAME --archive --delete --prune-empty-dirs \
       --exclude 'secure' \
       --exclude 'hardware-configuration.nix' \
       '/etc/nixos/' "$HOME/.files/etc/nixos"
 
+printf "Syncing $HOME/.config/nvim\n"
+
 # exclude everything but .fnl files in any directory in the
 # nvim one. do not include empty directories
-rsync -av --delete --prune-empty-dirs \
+rsync --info=NAME --archive --delete --prune-empty-dirs \
       --include '*.fnl' \
       --include 'syntax/*.vim' \
       --include "*/" \
