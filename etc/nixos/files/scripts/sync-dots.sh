2c6f3e0ef5d57ea3395b66f655967ed5488c6ca5
Hemant Sai Gouni <me@hgouni.com>
2023-07-03 06:48:23 +0000

    Fix dotfile sync script

--- etc/nixos/files/scripts/sync-dots.sh
+++ etc/nixos/files/scripts/sync-dots.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+# @ --exclude 'secure' \:
+# exclude files that should not be uploaded to git
+# like private keys for VPN configs
+# 
+# (this comment can't go in the rsync command itself, gets parsed wrong)
 rsync -av --delete \
-      # exclude files that should not be uploaded to git
-      # like private keys for VPN configs
       --exclude 'secure' \
       --exclude 'hardware-configuration.nix' \
       '/etc/nixos/' "$HOME/.files/etc/nixos"
