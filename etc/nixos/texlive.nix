d8b61c191a49d1e017d96dc12781f311a3c73bbd
Hemant Sai Gouni <me@hgouni.com>
2023-07-10 17:51:33 +0000

    Add keepassxc setup

--- etc/nixos/texlive.nix
+++ etc/nixos/texlive.nix
@@ -5,6 +5,7 @@
     pkgs.texlive.combined.scheme-full
   ];
 
+  # see latexmk -showextraoptions for a full list of options
   # pdf_mode = 4 sets lualatex as the default latex implementation
   home.file.".latexmkrc".text = ''
     $pdf_mode = 4;
