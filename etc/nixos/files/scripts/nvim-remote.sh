5be39767cf3a805d081912640e13fbdf68a6b726
Hemant Sai Gouni <me@hgouni.com>
2023-07-26 14:21:47 +0000

    Fix nvim-remote scripts; separate by '\0's

--- etc/nixos/files/scripts/nvim-remote.sh
+++ etc/nixos/files/scripts/nvim-remote.sh
@@ -1,5 +1,7 @@
 # double quoting $@ is special: quotes each entry in the list of arguments
-readlink -f "$@" | xargs nvim --server "$NVIM" --remote-tab
+# separate by null terminators instead of newlines to handle files w/ spaces
+readlink --canonicalize --zero "$@" \
+    | xargs --null nvim --server "$NVIM" --remote-tab
 
 # so the 'editor' waits for input
 read -rp $'Press enter to finish editing.\n'
