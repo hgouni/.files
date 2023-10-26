5eb99bc7cf0127f42217128713ab6447314e08a2
Hemant Sai Gouni <me@hgouni.com>
2023-10-26 07:04:04 +0000

    fancier bash prompt

--- etc/nixos/bash.nix
+++ etc/nixos/bash.nix
@@ -8,21 +8,32 @@
 
   programs.bash = {
     enable = true;
+    shellAliases = {
+      d = "pushd";
+      b = "popd";
+      c = "clear";
+      f = "ls";
+      e = "nvim";
+    };
     # login only
     profileExtra = ''
       if [[ "$-" == *i* && "$(tty)" == '/dev/tty1' ]]; then
           exec sway
       fi
     '';
-    # interactive only
-    initExtra = ''
-      alias d='pushd'
-      alias b='popd'
-      alias c='clear'
-      alias f='ls'
-      alias e='nvim'
 
-      PS1="\e[1;32m\j \w\e[m "
+    # interactive only
+    initExtra = let
+      red = ''\e[1;31m'';
+      green = ''\e[1;32m'';
+      clear = ''\e[m'';
+      improperExit = ''$(if [ "$?" -eq 0 ]; then printf "%s" "${green}"; else printf "%s" "${red}"; fi)'';
+      ssh = ''$([ -n "$SSH_TTY" ] && printf "%s " "\h")'';
+      jobs = ''$([ "\j" -gt 0 ] && printf "%s " "\j")'';
+      workdir = ''\w'';
+    in
+    ''
+      PS1=' ${improperExit}${ssh}${jobs}${workdir}${clear} '
     '';
   };
 }
