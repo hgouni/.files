b3908e8abc3e2c1a33e4cb0cd54800f19321c5c4
Hemant Sai Gouni <me@hgouni.com>
2026-09-01 00:47:04 +0000

    Enable offloading

--- etc/nixos/nvidia.nix
+++ etc/nixos/nvidia.nix
@@ -15,7 +15,10 @@
     open = true;
     modesetting.enable = true;
     prime = {
-      offload.enable = true;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
 
       intelBusId = "PCI:0@0:2:0";
       nvidiaBusId = "PCI:1@0:0:0";
